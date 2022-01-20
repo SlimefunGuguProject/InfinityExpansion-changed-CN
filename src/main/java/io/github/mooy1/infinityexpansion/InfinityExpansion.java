1eb044c6082ca63d38bc24dfc18b44e84f841b6a
ybw0014 <i@guizhanss.com>
2022-01-20 18:06:34 -0500

    更新汉化库

--- src/main/java/io/github/mooy1/infinityexpansion/InfinityExpansion.java
+++ src/main/java/io/github/mooy1/infinityexpansion/InfinityExpansion.java
@@ -42,7 +42,7 @@ public final class InfinityExpansion extends AbstractAddon {
     @Override
     protected void enable() {
         Metrics metrics = new Metrics(this, 8991);
-        String autoUpdates = String.valueOf(autoUpdatesEnabled());
+        String autoUpdates = String.valueOf(false);
         metrics.addCustomChart(new SimplePie("auto_updates", () -> autoUpdates));
 
         Plugin lx = getServer().getPluginManager().getPlugin("LiteXpansion");
--- src/main/java/io/github/mooy1/infinityexpansion/items/quarries/Oscillator.java
+++ src/main/java/io/github/mooy1/infinityexpansion/items/quarries/Oscillator.java
@@ -17,7 +17,7 @@ import io.github.thebusybiscuit.slimefun4.api.items.SlimefunItemStack;
 import io.github.thebusybiscuit.slimefun4.api.recipes.RecipeType;
 import io.github.thebusybiscuit.slimefun4.implementation.SlimefunItems;
 
-import net.guizhanss.minecraft.guizhanlib.minecraft.helper.MaterialHelper;
+import net.guizhanss.guizhanlib.minecraft.helper.MaterialHelper;
 
 public final class Oscillator extends SlimefunItem {
 
